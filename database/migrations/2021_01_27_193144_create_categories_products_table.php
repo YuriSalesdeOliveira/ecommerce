4c941552407cb5006d4014cbd5ed6d4fe9b935f5
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2021-01-27 19:17:22 -0300

    Migrations para product e category criados

---
+++ database/migrations/2021_01_27_193144_create_categories_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category');
+            $table->unsignedBigInteger('product');
+
+            $table->foreign('category')->references('id')->on('categories')->delete('CASCADE');
+            $table->foreign('product')->references('id')->on('products')->delete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories_products');
+    }
+}
