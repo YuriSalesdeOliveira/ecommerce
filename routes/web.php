f34e168e0158cd1d6a67bb8329ec65c8ccd56ba5
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2021-01-29 12:43:42 -0300

    Rotas da home para dashboard e site

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site\SiteController;
+use App\Http\Controllers\Dashboard\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,3 +14,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+/**
+ * Site
+ */
+
+Route::get('/', [SiteController::class, 'home'])->name('site.home');
+Route::get('/inicio', [SiteController::class, 'home'])->name('site.home');
+
+/**
+ * Dashboard
+ */
+
+Route::prefix('painel-de-controle')->group(function () {
+
+    Route::get('/', [DashboardController::class, 'home'])->name('dashboard.home');
+    Route::get('/inicio', [DashboardController::class, 'home'])->name('dashboard.home');
+
+});
